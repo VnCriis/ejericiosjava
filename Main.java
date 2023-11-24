b3a076fbcdb7a083befa13f177a3a7f4c67425e7
CarlosAUD <150806786+CarlosAUD@users.noreply.github.com>
2023-11-24 12:17:35 -0500

    Corregida la parte

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public Main(String[] args) {
+        double[][] calificacionesE = new double[10][5];
+        leerNotas(calificacionesE);
+        double[] promedioE = notaProm(calificacionesE);
+        mostrarP(promedioE);
+        verSuple(promedioE);
+    }
+
+    private static double[] notaProm(double[][] calificacionesE) {
+        return new double[0];
+    }
+
+    private static void mostrarP(double[] promedioE) {
+    }
+
+    private static void verSuple(double[] promedioE) {
+    }
+
+    private static void leerNotas(double[][] calificacionesE) {
+
+        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
+        for (int i = 0; i < calificacionesE.length; i++) {
+            System.out.println("Ingrese las calificaciones del estudiante " + (i + 1) + ":");
+            for (int j = 0; j < calificacionesE[i].length; j++) {
+                System.out.print("Ingrese la calificacion de la materia " + (j + 1) + ": ");
+                calificacionesE[i][j] = scanner.nextDouble();
+            }
+        }
+    }
+}
